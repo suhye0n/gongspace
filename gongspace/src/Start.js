2678e3e012e24b55f5841c8d942f8796dba2c73d
suhye0n <contact.suhyeon@gmail.com>
2022-04-26 14:33:22 +0900

    22.04.26 리액트 개발환경 설정

---
+++ gongspace/src/Start.js
@@ -0,0 +1,22 @@
+import './Start.css';
+
+function Start() {
+  return (
+    <div className="Start">
+      <header className="Start-header">
+          <div>마이페이지</div>
+      </header>
+
+      <ul className="Start-cate">
+          <li>planner</li>
+          <li>til</li>
+      </ul>
+
+      <article></article>
+
+      <footer></footer>
+    </div>
+  );
+}
+
+export default Start;
